0ae314728e90180f29ad338d5c2963be7f276452
Dung (Justin) Le <dle21@ssis.edu.vn>
2019-08-15 08:27:52 +0700

    Update fahrenheit.py

--- fahrenheit.py
+++ fahrenheit.py
@@ -3,5 +3,7 @@ while True:
         c = float(input("C: "))
         f = 1.8 * c + 32
         print("F:", f)
+        import sys
+        sys.exit()
     except ValueError:
         print("That's not a number.")
